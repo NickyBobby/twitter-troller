581859955d89efaf4eeda5fd491819b54d36bc6b
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-12-31 19:42:01 -0500

    Adds analytics and tweet output when done

--- steve_troller.rb
+++ steve_troller.rb
@@ -1,6 +1,5 @@
 require 'twitter'
 require_relative 'twitter_keys.rb'
-require 'pry'
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = CONSUMER_KEY
@@ -9,8 +8,9 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ACCESS_TOKEN_SECRET
 end
 
-
-# steve = client.user('stevekinney')
+retweet_count = 0
+reply_count   = 0
+tweet_count   = 0
 
 options = {
   count: 200
@@ -20,8 +20,17 @@ tweets = client.user_timeline('stevekinney', options)
 
 tweets.each do |tweet|
   if Date.today > Date.parse(tweet.created_at.to_s) && Date.today.prev_day.prev_day < Date.parse(tweet.created_at.to_s)
-    client.retweet(tweet)
+    client.retweet(tweet) rescue
+    client.favorite(tweet) rescue
     puts tweet.text
-
+    if tweet.reply?
+      reply_count += 1
+    elsif tweet.retweet?
+      retweet_count += 1
+    else
+      tweet_count += 1
+    end
   end
 end
+
+client.update("Done liking/retweeting Steve Kinney's tweets from yesterday: #{tweet_count} original tweets, #{reply_count} replies and #{retweet_count} retweets. #SubtlyTrollingSteve2017")
